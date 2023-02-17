043d214d6502b89eecea24a8c6196e519d450361
Amir Bazanov <flash5651311@gmail.com>
2023-02-17 17:53:44 +0500

    First homework

---
+++ first/work_with_strings.ts
@@ -0,0 +1,39 @@
+export const upperCase = (str: string): string => str[0].toUpperCase() + str.slice(1).toLowerCase();
+
+export const wordsCount = (str: string) => {
+    return str.split(' ').filter(function (num) {
+        return num != ''
+    }).length;
+}
+
+export const rightSpacing = (str: string) => {
+    const marks = [',', '.', '?', '!']
+    let convertedStr = ''
+    let prevChar = '';
+    str = str.replace(/\s\s+/g, ' ')
+    for (const char of str.split('')) {
+        if (!marks.includes(char)) {
+            convertedStr += char
+        } else {
+            if (prevChar == ' ') {
+                convertedStr = convertedStr.slice(0, convertedStr.length - 1)
+            }
+            convertedStr += char + ' '
+        }
+        prevChar = char
+    }
+    return convertedStr.replace(/\s\s+/g, ' ')
+}
+
+export const uniqWordsCount = (str: string) => {
+    let words = str.toLowerCase().replace(/,/g, '').split(" ")
+    let wordMap: { [key: string]: number } = {};
+
+    for (const word of words) {
+        let currentWordCount = wordMap[word];
+        let count = currentWordCount ? currentWordCount : 0;
+        wordMap[word] = count + 1;
+    }
+    return wordMap;
+}
+
